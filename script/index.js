e268a306d0bbb87f058b562e31ba84bbbd040846
louise-ulrika <ulrika.nicander@gmail.com>
2025-10-23 16:48:32 +0200

    changes thursday 23rd

--- script/index.js
+++ script/index.js
@@ -1,1 +1,17 @@
 console.log('Hello, World!');
+
+let name= 'Ulrika';
+
+let age=48;
+
+let color='green';
+
+
+
+console.log(`My name is ${name}, I am ${age} years old and my favorite color is ${color}.`);    
+
+let applePrice=4;
+
+let orangeprice=3;
+
+console.log(applePrice+= orangeprice, applePrice-=orangeprice, applePrice*=orangeprice,applePrice/=orangeprice);
